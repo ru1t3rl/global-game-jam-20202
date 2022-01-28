92c4a72230c862319a051e95f5f325df69055b57
xsedohrx <johnathan.claasen@hva.nl>
2022-01-28 22:06:20 +0100

    Added movement for character player

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCharacterController : MonoBehaviour
 {
+    CharacterController characterController;
+
+    [SerializeField] float speed = 6f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,23 @@ public class ThirdPersonCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+            characterController.Move(direction * speed * Time.deltaTime);
+
+        {
+
+        }
+
+
     }
 }
