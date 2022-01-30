7576e24f74dec986106159a2f5ccb66dc6882685
Lunar <tirsogcmeijer@gmail.com>
2022-01-30 13:24:21 +0100

    Added a jump and tweaked player physics

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -11,18 +11,38 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
     [SerializeField] float speed = 6f;
     [SerializeField] float turnSmoothTime = 0.1f, targetAngle, angle;
+    [SerializeField] float jumpForce = 10;
+    [SerializeField] float jumpGravityMultiplier = 5;
+    [SerializeField] float fallGravityMultiplier = 10;
     private float turnSmoothVelocity;
     private Camera cam;
+    private CapsuleCollider capsuleCollider;
+
+    private bool IsGrounded
+    {
+        get
+        {
+            return Physics.SphereCast(new Ray(transform.position + capsuleCollider.center, Vector3.down), capsuleCollider.radius, capsuleCollider.center.y);
+        }
+    }
 
     private void Awake()
     {
         //characterController = GetComponent<CharacterController>();
         cam = Camera.main;
         rigidBody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position + capsuleCollider.center, capsuleCollider.radius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(new Ray(transform.position + capsuleCollider.center, Vector3.down * (transform.position.y + capsuleCollider.center.y + 0.1f)));
+    }
+
+    private void Update()
     {
         MovePlayer();
     }
@@ -35,16 +55,26 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
     private void MovePlayer()
     {
+        bool jump = Input.GetButtonDown("Jump");
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 moveDirection = Vector3.zero;
 
         if (direction.magnitude >= 0.1f)
         {
-            Vector3 moveDirection = Quaternion.Euler(0f, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y, 0f) * Vector3.forward;
-            rigidBody.velocity += moveDirection.normalized * speed * Time.deltaTime;
+            moveDirection = Quaternion.Euler(0f, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y, 0f) * Vector3.forward;
         }
+        Vector3 moveVelocity = (moveDirection.normalized * speed * Time.deltaTime);
+        moveVelocity += (jump && IsGrounded) ? Vector3.up * jumpForce : Vector3.zero;
+        if (rigidBody.velocity.y <= 0)
+            moveVelocity += Physics.gravity * (fallGravityMultiplier - 1) * Time.deltaTime;
+        else
+            moveVelocity += Physics.gravity * (jumpGravityMultiplier - 1) * Time.deltaTime;
+
+        rigidBody.velocity += moveVelocity;
+
         RotatePlayer();
     }
 
