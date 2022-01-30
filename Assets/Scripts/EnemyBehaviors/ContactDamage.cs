495af0a7f042677ef643932b81a58a0e65296a15
Luca Ruiters <lucaruiters@hotmail.nl>
2022-01-30 13:56:05 +0100

    Added soundfx to the enemies

--- Assets/Scripts/EnemyBehaviors/ContactDamage.cs
+++ Assets/Scripts/EnemyBehaviors/ContactDamage.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ContactDamage : MonoBehaviour
 {
     [SerializeField]
     float damage, knockback;
 
+    public UnityEvent onContact;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Player player))
         {
             player.ApplyDamage(new DamageData(damage, knockback, gameObject, collision.GetContact(0).point));
+            onContact?.Invoke();
         }
     }
 }
